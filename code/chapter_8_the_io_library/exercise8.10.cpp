aac756c3b91626ead1c79a6ce41f8a34771cb161
yangle <yangle@luojilab.com>
2022-05-09 19:55:59 +0800

    3.2 string getline

--- code/chapter_8_the_io_library/exercise8.10.cpp
+++ code/chapter_8_the_io_library/exercise8.10.cpp
@@ -1,17 +1,32 @@
-Exercise 8.10:
-Write a program to store each line from a file in a vector<string>.
-Now use an istringstream to read each element from the vector a word at a time.
+// Exercise 8.10:
+// Write a program to store each line from a file in a vector<string>.
+// Now use an istringstream to read each element from the vector a word at a time.
+#include <iostream>
+#include <string>
+#include <vector>
+#include <sstream>
+#include <fstream>
+
+using std::cout;
+using std::endl;
+using std::vector;
+using std::string;
+using std::istringstream;
 
 int main()
 {
   vector<string> lines;
   string line;
-  ifstream ifs("resources/data.txt")
-  while (get_line(ifs, line))
+  ifstream ifs("resources/data.txt");
+  while (getline(ifs, line))
   {
     lines.push_back(line);
   }
-
-  
-  
+  for (auto s : lines)
+  {
+    istringstream iss(s);
+    string word;
+    iss >> word;
+    cout << word << endl;
+  }
 }
