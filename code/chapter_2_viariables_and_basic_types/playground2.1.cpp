957b380fbab467534a0739f134da74d237110eac
yangle <yangle@luojilab.com>
2022-05-10 14:47:46 +0800

    unsigned int

---
+++ code/chapter_2_viariables_and_basic_types/playground2.1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using std::cout; using std::endl;
+
+int main()
+{
+  unsigned a = 20;
+  int b = -5;
+  cout << a + b << endl;
+
+  unsigned u = 10;
+  int i = -42;
+  cout << i + u << endl;
+  
+  return 0;
+}
--- code/chapter_8_the_io_library/exercise8.11.cpp
+++ code/chapter_8_the_io_library/exercise8.11.cpp
@@ -5,30 +5,46 @@
 
 #include <string>
 #include <vector>
+#include <iostream>
+#include <sstream>
+
+using std::cout; using std::endl; using std::cin;
 
 using std::string;
 using std::vector;
+using std::istringstream;
 
 struct PersonInfo
 {
   string name;
   vector<string> phones;
-}
+};
 
 int main()
 {
   string line, word;
   vector<PersonInfo> people;
+  istringstream record;
   while (getline(cin, line))
   {
     PersonInfo info;
-    istringstream record(line);
+    record.clear();
+    record.str(line);
     record >> info.name;
     while (record >> word)
       info.phones.push_back(word);
     people.push_back(info);
   }
-  
+
+  for (auto &person : people)
+  {
+    cout << endl << "----------" << endl;
+    cout << "name: " << person.name << endl;
+    cout << "phones: " << endl;
+    for (auto &phone : person.phones)
+      cout << phone << " ";
+    cout << endl;
+  }
 
   return 0;
 }
