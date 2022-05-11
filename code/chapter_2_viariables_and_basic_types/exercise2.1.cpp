0b0bdcbe88359aa44982c45f9c825a11571dce70
yangle <yangle@luojilab.com>
2022-05-11 09:35:52 +0800

    arithmatic types

---
+++ code/chapter_2_viariables_and_basic_types/exercise2.1.cpp
@@ -0,0 +1,9 @@
+// Exercise 2.1:
+// What are the differences between int, long, long long, and short?
+// Between an unsigned and a signed type?
+// Between a float and a double?
+
+// int占有16bits, 2byte, long占有32bit, 4byte, long long占有64bit, 8byte  
+// signed可以表示正数、负数、0，unsigned只能表示正数、0  
+// float占有32bits, 4byte, double占有64bits, 8byte  
+// float最多6个significant digits, double最多10个significant digits
---
+++ code/chapter_2_viariables_and_basic_types/exercise2.2.cpp
@@ -0,0 +1,5 @@
+// Exercise 2.2:
+// To calculate a mortgage payment, what types would you use for the rate, principal, and payment?
+// Explain why you selected each type.
+
+// 用double, 因为精度足够
