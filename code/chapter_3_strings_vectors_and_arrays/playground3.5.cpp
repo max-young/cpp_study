b16b56744a5c0376e662b1ecf1289c32cfa77407
yangle <yangle@luojilab.com>
2022-05-19 09:59:46 +0800

    array

---
+++ code/chapter_3_strings_vectors_and_arrays/playground3.5.cpp
@@ -0,0 +1,14 @@
+# include <iostream>
+
+int main() {
+  int a[10], b[10];
+  for (std::size_t i = 0; i < 10; i++) {
+    a[i] = i * 2 - 1;
+    b[10 - i - 1] = a[i];
+  }
+  for (std::size_t i = 0; i < 10; i++) {
+    std::cout << "a[" << i << "] = " << a[i] << " ";
+    std::cout << "b[" << i << "] = " << b[i] << std::endl;
+  }
+  return 0;
+}
